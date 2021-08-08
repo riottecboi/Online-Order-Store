bd0c1a014c8edc87fef9447ac480d3789ef9ec63
tranvinhliem <liem@dfir.vn>
2021-08-08 21:55:30 +0700

    issue #3 - initial commit

--- frontend.py
+++ frontend.py
@@ -3,6 +3,7 @@ import mysql.connector
 from mysql.connector import pooling
 from flask_wtf.csrf import CSRFProtect
 from flask import Flask, render_template, redirect, url_for, make_response, request
+import uuid
 
 class Configuration(metaclass=MetaFlaskEnv):
     SECRET_KEY = "onlineshopsecretkey"
@@ -17,6 +18,7 @@ class Configuration(metaclass=MetaFlaskEnv):
     ITEMS_TABLE = "items"
     ORDERS_TABLE = "orders"
     UPLOAD_TABLE = "upload"
+    PRODUCTS_TABLE = "products"
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -51,6 +53,34 @@ def get_items(id):
         ret = {'id': record[0], 'title': record[1], 'description': record[2], 'price': record[3]}, 200
     else:
         ret = {}, 404
+    c.close()
+    conn.close()
+    return ret
+
+def update_product(products):
+    identified = str(uuid.uuid4())
+    conn = cnxpool.get_connection()
+    c = conn.cursor()
+    mysql_update_query = f"insert into {app.config['PRODUCTS_TABLE']} (itemid, quantity, price, identified) values (%s,%s,%s,%s)"
+    for product in products:
+        c.execute(mysql_update_query,(product['id'], product['quantity'], product['price'], identified))
+        conn.commit()
+    c.close()
+    conn.close()
+    return identified
+
+def update_order(identified, name, phone, email, address, city, payment, total):
+    conn = cnxpool.get_connection()
+    c = conn.cursor()
+    mysql_update_query = f"insert into {app.config['ORDERS_TABLE']} (identified, name, phone, email, address, city, payment, total) values (%s,%s,%s,%s,%s,%s,%s,%s)"
+    try:
+        c.execute(mysql_update_query, (identified, name, phone, email, address, city, payment, total))
+        conn.commit()
+        ret = 'Updated', 200
+    except Exception as e:
+        ret = str(e), 404
+    c.close()
+    conn.close()
     return ret
 
 @app.route('/')
@@ -72,20 +102,39 @@ def detail():
             datas.append(item[0])
     return render_template('product-details.html', items=datas)
 
+@app.route('/processing', methods=['GET','POST'])
+def processing():
+    identified = request.form.get('identified')
+    name = request.form.get('name')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    address = request.form.get('address')
+    city = request.form.get('city')
+    type = request.form.get('type')
+    total = request.form.get('price')
+    update = update_order(identified,name,phone,email,address,city,type,total)
+    if update[1] == 200:
+        return 'Done'
+
 
 @app.route('/check', methods=['POST'])
 def check():
     itemids = request.form.getlist('check')
     return redirect(url_for('detail', itemids=itemids))
 
-@app.route('/processing', methods=['POST'])
-def processing():
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    final_price = 0
     products = []
     quantity = request.form.getlist('quantity')
     item_id = request.form.getlist('item')
-    for i, q in zip(item_id, quantity):
-        products.append({'id': i, 'quantity': q})
-    return redirect(url_for('checkout', products=products))
+    price = request.form.getlist('price')
+    for i, q, p in zip(item_id, quantity, price):
+        price = int(q)*int(p)
+        final_price = final_price + price
+        products.append({'id': i, 'quantity': q, 'price':p})
+    identified = update_product(products)
+    return render_template('checkout.html', final_price=final_price, identified=identified)
 
 try:
     app.config.from_pyfile('settings.cfg')
--- sql/items.sql
+++ sql/items.sql
@@ -10,13 +10,15 @@ CREATE TABLE `items` (
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
+    `identified` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
     `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
     `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-    `phone` int(10) NOT NULL,
+    `phone` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
     `address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
     `city` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
     `payment` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-    `time` timestamp NOT NULL,
+    `total` int(10) NOT NULL,
+    `time` timestamp NULL,
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -26,4 +28,14 @@ CREATE TABLE `upload` (
     `filename` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
 KEY `itemid_idx` (`itemid`),
 CONSTRAINT `fk_itemidid` FOREIGN KEY (`itemid`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+    `itemid` int(10) NOT NULL AUTO_INCREMENT,
+    `quantity` int(4) NOT NULL,
+    `price` int(10) NOT NULL,
+    `identified` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+KEY `itemid_idx` (`itemid`),
+CONSTRAINT `fk_itemid` FOREIGN KEY (`itemid`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
